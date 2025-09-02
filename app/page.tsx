59bf5dfcad989f886e7298df244d787558000f32
Mohamad Syifa Ardian Rachman <ardianrachman9@gmail.com>
2025-09-02 23:22:38 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function App() {
             </div>
           )}
           {!isLoading && results.length > 0 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-6">
               {results.map((item) => (
                 <div
                   key={item.imdbID}
